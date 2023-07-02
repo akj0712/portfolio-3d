736dd1fc5b762b076bcd8db43ebd9935a95b92b4
Abhinav Jha <jhaabhinavkumar1@gmail.com>
2023-07-02 16:02:10 +0530

    assets

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import logo from "./akj.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import expressjs from "./tech/express.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import bootstrap from "./tech/bootstrap.png";
+import threejs from "./tech/threejs.svg";
+import postman from "./tech/postman.png";
+import cplusplus from "./tech/CPlusPlus.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import netflix from "./netflix.png";
+import booking from "./booking.png";
+import darksky from "./darksky.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  expressjs,
+  reactjs,
+  bootstrap,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  postman,
+  cplusplus,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  netflix,
+  booking,
+  darksky,
+};
